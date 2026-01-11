a2aeec819fa3564cf4421b617096b9e9bc898421
Hariharan <hariharan9572@gmail.com>
2026-01-11 22:45:34 +0530

    sending data and RequestBody

--- src/main/java/com/hariharan/springbootrest/JobRestController.java
+++ src/main/java/com/hariharan/springbootrest/JobRestController.java
@@ -4,10 +4,7 @@ import com.hariharan.springbootrest.model.JobPost;
 import com.hariharan.springbootrest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,10 @@ public class JobRestController {
     @GetMapping("jobPost/{postID}")
     public JobPost getJob(@PathVariable("postID") int postId){
         return service.getJob(postId);
+    }
+
+    @PostMapping("jobPost")
+    public void addJob(@RequestBody JobPost jobPost){
+        service.addJob(jobPost);
     }
 }
