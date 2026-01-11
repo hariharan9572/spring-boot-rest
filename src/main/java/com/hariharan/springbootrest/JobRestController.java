6b1c42b73e6172cd6f11395169c410c38ee47f6b
Hariharan <hariharan9572@gmail.com>
2026-01-11 22:08:59 +0530

    creating a rest controller

---
+++ src/main/java/com/hariharan/springbootrest/JobRestController.java
@@ -0,0 +1,23 @@
+package com.hariharan.springbootrest;
+
+import com.hariharan.springbootrest.model.JobPost;
+import com.hariharan.springbootrest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping("jobPosts")
+    @ResponseBody
+    public List<JobPost> getAllJobs(){
+        return service.getAllJobs();
+    }
+}
---
+++ src/main/java/com/hariharan/springbootrest/service/JobService.java
@@ -0,0 +1,23 @@
+package com.hariharan.springbootrest.service;
+
+import com.hariharan.springbootrest.model.JobPost;
+import com.hariharan.springbootrest.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost jobPost){
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs(){
+        return repo.getAllJobs();
+    }
+}
