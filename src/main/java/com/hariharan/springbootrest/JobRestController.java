1861ff211a6a3faecba2f846dd94392f6028ea9b
Hariharan <hariharan9572@gmail.com>
2026-01-11 22:19:15 +0530

    Connecting react and spring

--- src/main/java/com/hariharan/springbootrest/JobRestController.java
+++ src/main/java/com/hariharan/springbootrest/JobRestController.java
@@ -4,12 +4,14 @@ import com.hariharan.springbootrest.model.JobPost;
 import com.hariharan.springbootrest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobRestController {
 
     @Autowired
