274b9620baa50682dc00f98d181a734ae69a5ee7
Hariharan <hariharan9572@gmail.com>
2026-01-16 00:13:34 +0530

    Job App is secure now

---
+++ src/main/java/com/hariharan/springbootrest/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.hariharan.springbootrest.repo;
+
+import com.hariharan.springbootrest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
---
+++ src/main/java/com/hariharan/springbootrest/service/UserService.java
@@ -0,0 +1,22 @@
+package com.hariharan.springbootrest.service;
+
+import com.hariharan.springbootrest.model.User;
+import com.hariharan.springbootrest.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public User saveUser(User user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        System.out.println(user.getPassword());
+        return repo.save(user);
+    }
+
+}
